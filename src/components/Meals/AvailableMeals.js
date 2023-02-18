dd30d021ecb1cb95dc19d87b2b2797e99d748975
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-02-18 13:28:00 +0100

    handling error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,11 +5,20 @@ import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(null);
+
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://meal-app-3b67d-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        setHasError(true);
+        throw new Error("Something is wrong");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -23,9 +32,29 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHasError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  if (hasError) {
+    return (
+      <section className={classes.error_text}>
+        <p>{hasError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
